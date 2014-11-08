6e8cf3070bedb67154cbaba8d64a729c68a0797a
Llewellyn van der Merwe <llewellyn@vdm.io>
2014-11-09 01:03:01 +0200

    Remove some old code that was not used

--- mod_getbiblesearch.php
+++ mod_getbiblesearch.php
@@ -15,9 +15,5 @@ require_once __DIR__ . '/getbiblesearch.php';
 
 // load the module class
 $module = new getSearch($params);
-// set module params
-$mode 		= $params->get('mode');
-$search 	= $params->get('search_phrase');
-$version 	= $params->get('version');
 
 require JModuleHelper::getLayoutPath('mod_getbiblesearch', $params->get('layout', 'default'));
